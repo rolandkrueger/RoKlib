4f1124c0de6192185b166b011f5b7b037d784052
rolandkrueger <info@rolandkrueger.info>
2013-02-17 15:33:19 +0100

    Moved filtering table classes to incubator project.

--- RoKlib/src/main/java/org/roklib/ui/swing/table/filtertable/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Model classes for tables which support instant column-based data filtering when typing search terms. 
- */
-package org.roklib.ui.swing.table.filtertable;
--- RoKlib/src/main/java/org/roklib/ui/swing/table/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Java Swing table classes which support instant column-based data filtering when typing search terms.
- */
-package org.roklib.ui.swing.table;
