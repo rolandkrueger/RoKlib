15de76b186fdf632757bba07b389cceaa0461d44
Roland Krüger <info@rolandkrueger.info>
2014-08-30 22:16:23 +0200

    unit test for URIActionMapperTree#getRootActionMapper

--- RoKlib/src/test/java/org/roklib/webapps/uridispatching/URIActionMapperTreeTest.java
+++ RoKlib/src/test/java/org/roklib/webapps/uridispatching/URIActionMapperTreeTest.java
@@ -21,15 +21,34 @@
 package org.roklib.webapps.uridispatching;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.isA;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.roklib.webapps.uridispatching.URIActionMapperTree.*;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class URIActionMapperTreeTest {
+
+    @Mock
+    private AbstractURIActionCommand homeCommandMock;
 
     @Test
     public void test_create_returns_builder() {
-        assertThat(URIActionMapperTree.create(), isA(URIActionMapperTree.URIActionMapperTreeBuilder.class));
+        assertThat(create(), isA(URIActionMapperTreeBuilder.class));
+    }
+
+    @Test
+    public void test_getRootActionMapper() {
+        // @formatter:off
+        final URIActionMapperTree tree = create()
+                .map(pathSegment("home")
+                .on(action(homeCommandMock)))
+                .build();
+        // @formatter:on
+        assertThat(tree.getRootActionMapper("home").getActionCommand(), equalTo(homeCommandMock));
     }
 }
